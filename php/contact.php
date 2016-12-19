b17e17103ac88808a00d7002a0812930dee72bde
Danielle Walker <danielle@bliss.net.au>
2016-12-19 18:00:17 +1100

    PHP code working for relaying email thru Gmail SMTP

--- php/contact.php
+++ php/contact.php
@@ -1,40 +1,38 @@
 <?php
-// if(isset($_POST['submit']))
-{
-$message=
-'First Name:	'.$_POST['firstname'].'<br />
-Last Name:	'.$_POST['lastname'].'<br />
-Phone:	'.$_POST['phone'].'<br />
-Email:	'.$_POST['emailid'].'<br />
-Comments:	'.$_POST['message'].'
-';
-    require_once ("class.phpmailer.php"); //include phpmailer class
 
-    // Instantiate Class
-    $mail = new PHPMailer();
+  // Support for this PHP code & using PHPMailer can be found here:
+  // http://stackoverflow.com/questions/16048347/send-email-using-gmail-smtp-server-through-php-mailer
+  // Note: Angular or AJAX should be used on the front end to execute the PHP Script.
 
-    // Set up SMTP
-    $mail->IsSMTP();                // Sets up a SMTP connection
-    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization
-    $mail->SMTPSecure = "ssl";      // Connect using a TLS connection
-    $mail->Host = "smtp-relay.gmail.com";  //Gmail SMTP server address
-    $mail->Port = 465;  //Gmail SMTP port
-    $mail->Encoding = '7bit';
+  require_once ("class.phpmailer.php");   // Include phpmailer class
 
-    // Authentication
-    $mail->Username   = "webmaster@openhousecic.org.au"; // Your full Gmail address
-    $mail->Password   = "*******"; // Your Gmail password
+  $message=
+  'First Name:	'.$_POST['firstname'].'<br />
+  Last Name:	'.$_POST['lastname'].'<br />
+  Phone:	'.$_POST['phone'].'<br />
+  Email:	'.$_POST['emailid'].'<br />
+  Comments:	'.$_POST['message'].'
+  ';
 
-    // Compose
-    $mail->SetFrom($_POST['emailid'], $_POST['firstname'], $_POST['lastname']);
-    $mail->AddReplyTo($_POST['emailid'], $_POST['firstname'], $_POST['lastname']);
-    $mail->Subject = "New Contact Form Enquiry";      // Subject (which isn't required)
-    $mail->MsgHTML($message);
+  $mail = new PHPMailer();        // Instantiate the PHPMailer Class
+  $mail->IsSMTP();                // enable SMTP
+  $mail->SMTPDebug = 1;           // debugging: 1 = errors and messages, 2 = messages only
+  $mail->SMTPAuth = true;         // SMTP authentication enabled
+  $mail->SMTPSecure = 'ssl';      // secure transfer enabled + REQUIRED for Gmail (either SSL or TLS)
+  $mail->Host = "smtp.gmail.com"; //Gmail SMTP Server to relay thru
+  $mail->Port = 465; // Port 465 as we're using SSL... or use Port 587 for TLS
+  $mail->IsHTML(true);                               // We're sending a HTML formatted message
+  $mail->Username = "webmaster@openhousecic.org.au"; // Gmail account for authentication
+  $mail->Password = "*********";                     // Gmail password for authentication
+  $mail->SetFrom("webmaster@openhousecic.org.au");   // The email is being sent from this address
+  $mail->Subject = "Website Contact Form Enquiry";   // The subject line of the email
+  $mail->Body = ($message);                          // The actual email message to be sent
+  $mail->AddAddress("office@openhousecic.org.au"); // The email is being sent to this address
 
-    // Send To
-    $mail->AddAddress("info@bliss.net.au", "Web Contact"); // Where to send it - Recipient
-    $result = $mail->Send();		// Send!
-	$message = $result ? '<div class="alert alert-success" role="alert"><strong>Success!</strong>Message Sent Successfully!</div>' : '<div class="alert alert-danger" role="alert"><strong>Error!</strong>There was a problem delivering the message.</div>';
-	unset($mail);
-}
-?>
+ if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+ } else {
+    echo "Message has been sent";
+ }
+
+ ?>
