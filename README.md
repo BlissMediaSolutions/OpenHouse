d273a4f2d57e929184184e4a7449f7571094a144
Danielle Walker <danielle@bliss.net.au>
2017-01-05 17:46:50 +1100

    Completed Contact Page with working Angular Controller + AJAX

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 
 Status: Incomplete, Under Development
 
-Developed with HTML5, CSS3, Angular.js/Bootstrap, javascript
+Developed with/for: HTML5, CSS3, Bootstrap v3, Angular, Javascript/JQuery, PHP 5.6
 
-##### Tested in: 
+##### Tested in:
 - Firefox 49.0.2
 - Chrome 54.0.2
 - Internet Explorer 11.0.9600
@@ -15,7 +15,9 @@ Developed with HTML5, CSS3, Angular.js/Bootstrap, javascript
 ##### Notes:
 - Website uses Google Maps API v3 - which requires a developer API Key (not included).
 - HTML templates currently contain Javascript code, which should really be contained within the Angular Controllers.. (but hey, im just learning Angular, so its a dirty implementation/hack just to get the website functioning in a limited time frame).
-- Website embeds a Google Calendar via an iFrame.  Includes CSS code to make Calendar siZing responsive.
+- Website embeds a Google Calendar via an iFrame.  Includes CSS code to make Calendar sizing responsive.
+- Website uses NanoGallery to display images from either Flickr or Google Photo's.
+- Website Contact Form uses PHPMailer to relay an email thru GMail.  Due to SMTP headers being returned from PHP packaged with the data response for AJAX, JSON is converted to string in Angular & some simple string manipulation will remove the SMTP headers leaving us with the actual data response to display via Angular & DOM.
 
 ##### Developed by:
 - Danielle Walker
--- php/contact.php
+++ php/contact.php
@@ -4,38 +4,50 @@
   // http://stackoverflow.com/questions/16048347/send-email-using-gmail-smtp-server-through-php-mailer
   // Note: Angular or AJAX should be used on the front end to execute the PHP Script.
 
+  require_once ("class.phpmailer.php");   // Include phpmailer class
   ini_set('display_errors', 'On');
   error_reporting(E_ALL | E_STRICT);
 
-  require_once ("class.phpmailer.php");   // Include phpmailer class
+  $param = file_get_contents("php://input");
+  $paramObject = json_decode($param);
 
-  $message=
-  'First Name:	'.$_POST['firstname'].'<br />
-  Last Name:	'.$_POST['lastname'].'<br />
-  Phone:	'.$_POST['phone'].'<br />
-  Email:	'.$_POST['emailid'].'<br />
-  Comments:	'.$_POST['message'].'
-  ';
+  $firstname = $paramObject->firstname;
+  $lastname = $paramObject->lastname;
+  $email = $paramObject->emailid;
+  $phone = $paramObject->phoneno;
+  $message = $paramObject->message;
 
-  $mail = new PHPMailer();        // Instantiate the PHPMailer Class
-  $mail->IsSMTP();                // enable SMTP
-  $mail->SMTPDebug = 1;           // debugging: 1 = errors and messages, 2 = messages only
-  $mail->SMTPAuth = true;         // SMTP authentication enabled
-  $mail->SMTPSecure = 'ssl';      // secure transfer enabled + REQUIRED for Gmail (either SSL or TLS)
-  $mail->Host = "smtp.gmail.com"; //Gmail SMTP Server to relay thru
-  $mail->Port = 465; // Port 465 as we're using SSL... or use Port 587 for TLS
-  $mail->IsHTML(true);                               // We're sending a HTML formatted message
-  $mail->Username = "webmaster@openhousecic.org.au"; // Gmail account for authentication
-  $mail->Password = "AD624or325";                     // Gmail password for authentication
-  $mail->SetFrom("webmaster@openhousecic.org.au");   // The email is being sent from this address
-  $mail->Subject = "Website Contact Form Enquiry";   // The subject line of the email
-  $mail->Body = ($message);                          // The actual email message to be sent
-  $mail->AddAddress("webmaster@openhousecic.org.au"); // The email is being sent to this address
+    $message=
+     'First Name:    '.$firstname.'<br />
+     Last Name:  '.$lastname.'<br />
+     Phone:  '.$phone.'<br />
+     Email:  '.$email.'<br />
+     Comments:   '.$message.'
+     ';
 
- if(!$mail->Send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
- } else {
-    echo "Message has been sent";
- }
+     $mail = new PHPMailer();        // Instantiate the PHPMailer Class
+     $mail->IsSMTP();                // enable SMTP
+     $mail->SMTPDebug = 1;           // debugging: 1 = errors and messages, 2 = messages only
+     $mail->SMTPAuth = true;         // SMTP authentication enabled
+     $mail->SMTPSecure = 'ssl';      // secure transfer enabled + REQUIRED for Gmail (either SSL or TLS)
+     $mail->Host = "smtp.gmail.com"; //Gmail SMTP Server to relay thru
+     $mail->Port = 465; // Port 465 as we're using SSL... or use Port 587 for TLS
+     $mail->IsHTML(true);                               // We're sending a HTML formatted message
+     $mail->Username = "webmaster@openhousecic.org.au"; // Gmail account for authentication
+     $mail->Password = "AD624or325";                     // Gmail password for authentication
+     $mail->SetFrom("webmaster@openhousecic.org.au");   // The email is being sent from this address
+     $mail->Subject = "Website Contact Form Enquiry";   // The subject line of the email
+     $mail->Body = ($message);                          // The actual email message to be sent
+     $mail->AddAddress("office@openhousecic.org.au"); // The email is being sent to this address
 
- ?>
+    if(!$mail->Send()) {
+      $data = "";
+      $data = array('success' => false, 'comment' => 'Message could not be sent. Mailer Error: ' . $mail->ErrorInfo);
+      echo json_encode($data);
+      exit;
+    } else {
+      $data = "";
+      $data = array('success' => true, 'comment' => 'Thanks! We have received your message, and will contact you shortly');
+      echo json_encode($data);
+    }
+  ?>
